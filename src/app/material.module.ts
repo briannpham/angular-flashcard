9a89b877153548201f2d690467650dd2196859ed
Brian Pham <br.pham13@gmail.com>
2023-01-31 15:41:57 -0800

    setup material module for angular

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+	exports: [
+		MatButtonModule,
+		MatListModule,
+		MatIconModule
+	]
+})
+
+export class MaterialModule {}
